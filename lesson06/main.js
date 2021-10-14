bb920ac6caea4ac1cf69c20e4bf4a414577cc39c
iLuvYouMegaHomo <megaluigi2804@gmail.com>
2021-10-14 23:23:09 +0700

    done work 2

--- lesson06/main.js
+++ lesson06/main.js
@@ -21,6 +21,7 @@ function filterOutFemales(arr){
 output1 = members.filter(element => element.gender != 'female');
 output2 = filterOutFemales(members)
 
+console.log('//BÀI 1//')
 console.log(output1)
 console.log(output2)
 
@@ -28,3 +29,44 @@ console.log(output2)
 /*                  BÀI 2                   */
 //////////////////////////////////////////////
 
+work2Arr = [1, 2, 3, 4, 7];
+work2Arr1 = [1, 2, 3, 4, 5, 6, 7];
+
+function diffArr(arr1, arr2){
+    var output = [];
+    //hack in order to use the longer array 
+    if (arr1.length >= arr2.length){
+        arr1.forEach(element => {
+            if(!arr2.includes(element)){
+                output.push(element);
+            }
+        })
+        return output;
+    } else {
+        arr2.forEach(element => {
+            if(!arr1.includes(element)){
+                output.push(element);
+            }
+        })
+        return output;
+    }   
+}
+
+
+outputWork2 = diffArr(work2Arr, work2Arr1);
+console.log('//BÀI 2//')
+console.log(outputWork2);
+
+
+//////////////////////////////////////////////
+/*                  BÀI 3                   */
+//////////////////////////////////////////////
+
+work3Arr = [
+    [1, 2, 3, 4, 5],
+    [5, 6, 7, 8, 9],
+    [20, 21, 34, 56, 100]
+];
+
+console.log('//BÀI 3//')
+work3Arr.forEach(element => console.log(element))
